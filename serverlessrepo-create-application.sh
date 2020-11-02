fa70d9fd3ced7aec8eeb20c33d513f0b0fc2b735
Mike Friesen <mfriesen@gmail.com>
2020-11-01 20:51:56 -0600

    Added distribution scripts

--- serverlessrepo-create-application.sh
+++ serverlessrepo-create-application.sh
@@ -3,7 +3,7 @@
 s3bucket=
 
 aws cloudformation package --template-file template.yaml --s3-bucket ${s3bucket} > template.tmp
-declare -a StringArray=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-east-1" "ap-east-1" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "me-south-1" "sa-east-1")
+declare -a StringArray=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "sa-east-1")
  
 # Iterate the string array using for loop
 for region in ${StringArray[@]}; do
@@ -18,5 +18,6 @@ for region in ${StringArray[@]}; do
      --license-body file://LICENSE \
      --readme-body file://README.md \
      --semantic-version "0.76.3" \
+     --spdx-license-id "MIT" \
      --template-body file://template.tmp
 done
